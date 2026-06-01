fedbb6631606e254cd215a89fcae0d7a4724e8fa
Ritik Shrivastava <shriva23@purdue.edu>
2026-06-01 11:10:47 -0500

    added a component for the album search logic

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+
+import { Album } from './models/album.model';
+import { ItunesService } from './services/itunes.service';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +10,45 @@ import { Component, signal } from '@angular/core';
   styleUrl: './app.scss',
 })
 export class App {
+  private readonly itunesService = inject(ItunesService);
+
   protected readonly title = signal('antra-project');
+
+  searchTerm = '';
+  searchedArtist = '';
+  albums: Album[] = [];
+  isLoading = false;
+  errorMessage = '';
+  hasSearched = false;
+
+  onSearch(): void {
+    const trimmedArtist = this.searchTerm.trim();
+
+    if (!trimmedArtist) {
+      this.albums = [];
+      this.searchedArtist = '';
+      this.errorMessage = 'Please enter an artist name.';
+      this.hasSearched = false;
+      return;
+    }
+
+    this.isLoading = true;
+    this.errorMessage = '';
+
+    this.itunesService.searchAlbums(trimmedArtist).subscribe({
+      next: (response) => {
+        this.albums = response.results;
+        this.searchedArtist = trimmedArtist;
+        this.hasSearched = true;
+        this.isLoading = false;
+      },
+      error: () => {
+        this.albums = [];
+        this.searchedArtist = trimmedArtist;
+        this.errorMessage = 'Unable to load albums. Please try again.';
+        this.hasSearched = true;
+        this.isLoading = false;
+      },
+    });
+  }
 }
