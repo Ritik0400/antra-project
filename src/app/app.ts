fb4c6c70cdc43310e5bd5d977c4da0cb2ec07262
Ritik Shrivastava <shriva23@purdue.edu>
2026-06-01 11:51:09 -0500

    added search rendering and style of album search layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, signal } from '@angular/core';
 
 import { Album } from './models/album.model';
 import { ItunesService } from './services/itunes.service';
@@ -11,6 +11,7 @@ import { ItunesService } from './services/itunes.service';
 })
 export class App {
   private readonly itunesService = inject(ItunesService);
+  private readonly changeDetector = inject(ChangeDetectorRef);
 
   protected readonly title = signal('antra-project');
 
@@ -21,7 +22,10 @@ export class App {
   errorMessage = '';
   hasSearched = false;
 
-  onSearch(): void {
+  onSearch(event?: Event): void {
+    event?.preventDefault();
+    event?.stopPropagation();
+
     const trimmedArtist = this.searchTerm.trim();
 
     if (!trimmedArtist) {
@@ -29,25 +33,27 @@ export class App {
       this.searchedArtist = '';
       this.errorMessage = 'Please enter an artist name.';
       this.hasSearched = false;
+      this.isLoading = false;
+      this.changeDetector.detectChanges();
       return;
     }
 
+    this.searchedArtist = trimmedArtist;
+    this.hasSearched = true;
     this.isLoading = true;
     this.errorMessage = '';
 
     this.itunesService.searchAlbums(trimmedArtist).subscribe({
       next: (response) => {
         this.albums = response.results;
-        this.searchedArtist = trimmedArtist;
-        this.hasSearched = true;
         this.isLoading = false;
+        this.changeDetector.detectChanges();
       },
       error: () => {
         this.albums = [];
-        this.searchedArtist = trimmedArtist;
         this.errorMessage = 'Unable to load albums. Please try again.';
-        this.hasSearched = true;
         this.isLoading = false;
+        this.changeDetector.detectChanges();
       },
     });
   }
